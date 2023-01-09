2ba7c9de8c1523b7071d9dd9e857db5299c8160e
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-01-10 01:00:50 +0900

    feat: 파일 업로드 다운르도 이미지 로드

--- src/main/java/com/codevumc/codev_backend/controller/co_project/CoProjectController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_project/CoProjectController.java
@@ -1,6 +1,13 @@
 package com.codevumc.codev_backend.controller.co_project;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CoProjectController {
+
+    private final FileService fileService;
+
+
 
 
 }
--- src/main/java/com/codevumc/codev_backend/domain/CoPhotoOfProject.java
+++ src/main/java/com/codevumc/codev_backend/domain/CoPhotoOfProject.java
@@ -2,10 +2,12 @@ package com.codevumc.codev_backend.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,10 +16,10 @@ public class CoPhotoOfProject {
     private long co_projectId;
     private String co_uuId;
     private String co_fileName;
-    private String co_url;
+    //private String co_url;
     private String co_filePath;
-    private String fileDownloadPath;
-    private Double co_fileSize;
+    private String co_fileDownloadPath;
+    private long co_fileSize;
     private Timestamp createdAt;
 
 }
