997d2057a320a549cce10305e69559a8c64c1019
simhani1 <simhani1@gmail.com>
2023-01-15 03:44:02 +0900

    fix: 프로젝트 사용 언어 배열 저장

--- src/main/java/com/codevumc/codev_backend/service/co_project/CoProjectServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_project/CoProjectServiceImpl.java
@@ -16,16 +16,18 @@ import org.springframework.stereotype.Service;
 public class CoProjectServiceImpl extends ResponseService implements CoProjectService {
     private final CoProjectMapper coProjectMapper;
 
-    public void insertProject(CoProject coProject, long co_languageId, JSONArray co_parts) throws ParseException {
+    public void insertProject(CoProject coProject, JSONArray co_languages, JSONArray co_parts) throws ParseException {
         this.coProjectMapper.insertCoProject(coProject);
         //this.coProjectMapper.insertCoLanguageOfProject(coProject.getCo_projectId(), co_languageId);
         JSONObject jsonObj;
+        for (Object co_language : co_languages) {
+            long co_languageId = Integer.parseInt(co_language.toString());
+            this.coProjectMapper.insertCoLanguageOfProject(coProject.getCo_projectId(), co_languageId);
+        }
         for (Object co_part : co_parts) {
             jsonObj = (JSONObject) co_part;
             this.coProjectMapper.insertCoPartOfProject(coProject.getCo_projectId(), Long.parseLong(jsonObj.get("co_partId").toString()), Long.parseLong(jsonObj.get("co_limit").toString()));
         }
-
-
     }
 
 //    public CoProject getCoProject(long co_projectId) {
