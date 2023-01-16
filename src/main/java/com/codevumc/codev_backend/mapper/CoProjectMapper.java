17b894cdcf6246ca648bf0369bbfe54185844eaf
min9yu98 <kimmingyu9811@gmail.com>
2023-01-16 22:10:44 +0900

    feat: 프로젝트 상세조회

--- src/main/java/com/codevumc/codev_backend/mapper/CoProjectMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoProjectMapper.java
@@ -1,16 +1,24 @@
 package com.codevumc.codev_backend.mapper;
 
-import com.codevumc.codev_backend.domain.CoProject;
+import com.codevumc.codev_backend.domain.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Mapper
 public interface CoProjectMapper {
     void insertCoProject(CoProject coProject);
     void insertCoPartOfProject(Map<String, Object> coPartsDto);
     void insertCoLanguageOfProject(@Param("co_projectId") long co_projectId, @Param("co_languageId") long co_languageId);
+    Optional<CoProject> getCoProject(@Param("co_projectId") long co_projectId);
 
+    List<CoPart> getCoPartList(long co_projectId);
+
+    List<CoLanguage> getCoLanguageList(long co_projectId);
+
+    long getCoHeartCount(long co_projectId);
 }
