a3e7ecf45225c68ff3467ca95a6d9d7707910ab6
min9yu98 <kimmingyu9811@gmail.com>
2023-01-16 22:10:44 +0900

    feat: 프로젝트 상세조회

--- src/main/java/com/codevumc/codev_backend/domain/CoPartOfProject.java
+++ src/main/java/com/codevumc/codev_backend/domain/CoPartOfProject.java
@@ -4,17 +4,12 @@ import lombok.*;
 
 import java.sql.Timestamp;
 
-@Setter
-@Getter
 @Builder
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CoPartOfProject {
-    private long co_popId;
-    private long co_projectId;
-    private long co_partId;
+    private String co_part;
     private int co_limit;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
-    private boolean status;
 }
