849a1444e7c5f5fa5a211898446b906cbf7fb1bf
pinetree2 <pinetree3966@gmail.com>
2023-01-16 19:44:07 +0900

    fix: 찜하기 취소 삭제로 변경

--- src/main/java/com/codevumc/codev_backend/controller/co_project/CoProjectController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_project/CoProjectController.java
@@ -81,7 +81,7 @@ public class CoProjectController extends JwtController {
     @PatchMapping("heart/cancel/{co_projectId}")
     public CoDevResponse heartOfProjectCancle(HttpServletRequest request,  @PathVariable("co_projectId") Long co_projectId) throws Exception {
         String co_email = getCoUserEmail(request);
-        return coProjectHeartService.updateHeart(co_email,co_projectId);
+        return coProjectHeartService.deleteHeart(co_email,co_projectId);
     }
 }
 
