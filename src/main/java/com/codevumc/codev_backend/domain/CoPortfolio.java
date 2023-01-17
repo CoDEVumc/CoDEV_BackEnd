d763115a29366ef16c212b358a162762cc3b98ce
min9yu98 <kimmingyu9811@gmail.com>
2023-01-17 15:56:54 +0900

    feat: portfolio 도메인 추가

--- src/main/java/com/codevumc/codev_backend/domain/CoPortfolio.java
+++ src/main/java/com/codevumc/codev_backend/domain/CoPortfolio.java
@@ -2,10 +2,27 @@ package com.codevumc.codev_backend.domain;
 
 import lombok.*;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CoPortfolio {
+    private long co_portfolioId;
+    private long co_lopfId;
+    private String co_email;
+    private String co_name;
+    private String co_gender;
+    private String co_birth;
+    private String co_rank;
+    private String co_headLine;
+    private String co_introduction;
+    private List<String> co_portfolioLanguage;
+    private List<String> co_portfolioLink;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private boolean status;
 }
