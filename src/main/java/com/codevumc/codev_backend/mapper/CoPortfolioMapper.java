9858e614511c5192038733ca0ca3eb47537e31f1
min9yu98 <kimmingyu9811@gmail.com>
2023-01-17 16:41:14 +0900

    feat: 포트폴리오 작성 미완

--- src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
@@ -1,7 +1,9 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoPortfolio;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CoPortfolioMapper {
+    void insertCoPortfolio(CoPortfolio coPortfolio);
 }
