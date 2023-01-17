9183d80b59366386bb86435a69ae0b77811dfbfc
pinetree2 <pinetree3966@gmail.com>
2023-01-17 20:21:29 +0900

    feat 1차 기능 개발 - 쿼리제외

--- src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
@@ -1,7 +1,12 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoPortfolio;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
 
 @Mapper
 public interface CoPortfolioMapper {
+
+    void updateCoPortfolio(CoPortfolio coPortfolio);
 }
