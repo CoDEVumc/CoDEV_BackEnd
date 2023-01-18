28f28de4fb5220a02fe3a7966b966f6ac05870c7
pinetree2 <pinetree3966@gmail.com>
2023-01-19 02:28:38 +0900

    fix : json array 오류 수정

--- src/main/java/com/codevumc/codev_backend/controller/co_mypage/CoMyPageController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_mypage/CoMyPageController.java
@@ -8,7 +8,7 @@ import com.codevumc.codev_backend.service.co_mypage.CoMyPageServiceImpl;
 import com.codevumc.codev_backend.service.co_portfolio.CoPortfolioServiceImpl;
 import com.codevumc.codev_backend.service.co_user.JwtService;
 import com.google.gson.Gson;
-import org.json.JSONArray;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +30,7 @@ public class CoMyPageController extends JwtController {
      *
      * */
     @ResponseBody
-    @PatchMapping("/{co_portfolioId}")
+    @PatchMapping("/update/{co_portfolioId}")
     public CoDevResponse updateCoPortfolio(HttpServletRequest request, @PathVariable("co_portfolioId") Long co_portfolioId, @RequestBody Map<String,Object> portfolio) throws Exception{
 
             //유저 email 일치한지 확인 코드 필요
@@ -38,21 +38,19 @@ public class CoMyPageController extends JwtController {
         CoPortfolio coPortfolio = CoPortfolio.builder()
                 .co_portfolioId(co_portfolioId)
                 .co_email(getCoUserEmail(request))
-                .co_name(portfolio.get("co_name").toString())
-                .co_gender(portfolio.get("co_gender").toString())
-                .co_birth(portfolio.get("co_birth").toString())
+                .co_title(portfolio.get("co_title").toString())
                 .co_rank(portfolio.get("co_rank").toString())
                 .co_headLine(portfolio.get("co_headLine").toString())
                 .co_introduction(portfolio.get("co_introduction").toString()).build();
-            //title추가해야함
+
 
         JSONParser parser = new JSONParser();
         Gson gson = new Gson();
         String co_portfolioLinks = gson.toJson(portfolio.get("co_link"));
         JSONArray co_portfoliolinksList = (JSONArray) parser.parse(co_portfolioLinks);
         String co_portfolioLanguages = gson.toJson(portfolio.get("co_portfolioLanguages"));
-        JSONArray co_portfolioLaguagesList = (JSONArray) parser.parse(co_portfolioLanguages);
-        this.coMyPageService.updateCoPortfolio(coPortfolio,co_portfoliolinksList,co_portfolioLaguagesList);
+        JSONArray co_portfolioLanguagesList = (JSONArray) parser.parse(co_portfolioLanguages);
+        this.coMyPageService.updateCoPortfolio(coPortfolio,co_portfoliolinksList,co_portfolioLanguagesList);
 
         return null;
     }
