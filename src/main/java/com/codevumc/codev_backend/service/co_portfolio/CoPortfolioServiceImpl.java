f0d8e45a3cb8ce6eb9bbe7bbed45dc194ea5cbc0
min9yu98 <kimmingyu9811@gmail.com>
2023-01-19 03:13:58 +0900

    feat: 포트폴리오 작성 완벽 완성

--- src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioServiceImpl.java
@@ -18,14 +18,10 @@ public class CoPortfolioServiceImpl extends ResponseService implements CoPortfol
     @Override
     public void insertCoPortfolio(CoPortfolio coPortfolio, JSONArray co_languages, JSONArray co_links) {
         this.coPortfolioMapper.insertCoPortfolio(coPortfolio);
-
         for (Object co_language : co_languages) {
             long co_languageId = (long) co_language;
             this.coPortfolioMapper.insertCoLanguageOfPortfolio(coPortfolio.getCo_portfolioId(), co_languageId);
         }
-
-        Map<String, Object> coPortfolioDto = new HashMap<>();
-
         for (Object co_plink : co_links) {
             String co_link = (String) co_plink;
             this.coPortfolioMapper.insertCoLinkOfPortfolio(coPortfolio.getCo_portfolioId(), co_link);
