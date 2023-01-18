b1be23384f6ed2c1e62cdb6c42d71b0a8142ed31
pinetree2 <pinetree3966@gmail.com>
2023-01-19 01:09:18 +0900

    feat : json 배열 읽기/db에 쓰기 처리

--- src/main/java/com/codevumc/codev_backend/mapper/CoMyPageMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoMyPageMapper.java
@@ -2,10 +2,13 @@ package com.codevumc.codev_backend.mapper;
 
 import com.codevumc.codev_backend.domain.CoPortfolio;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 @Mapper
 public interface CoMyPageMapper {
     void updateCoPortfolio(CoPortfolio coPortfolio);
-    void updateCoLaguageOfPortfolio(CoPortfolio coPortfolio);
-    void updateCoLinkOfPortfolio(CoPortfolio coPortfolio);
+    void updateCoLaguageOfPortfolio(@Param("co_portfolioId") long co_portfolioId, @Param("co_laugagueId") long co_laugageId);
+    void updateCoLinkOfPortfolio(Map<String, Object> coPortfolioDto);
 }
--- src/main/java/com/codevumc/codev_backend/service/co_mypage/CoMyPageServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_mypage/CoMyPageServiceImpl.java
@@ -6,6 +6,8 @@ import com.codevumc.codev_backend.mapper.CoMyPageMapper;
 import com.codevumc.codev_backend.mapper.CoPortfolioMapper;
 import com.codevumc.codev_backend.service.ResponseService;
 import lombok.AllArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -20,16 +22,27 @@ public class CoMyPageServiceImpl extends ResponseService implements CoMyPageServ
 
 
     @Override
-    public CoDevResponse updateCoPortfolio(CoPortfolio coPortfolio) {
+    public CoDevResponse updateCoPortfolio(CoPortfolio coPortfolio, JSONArray co_portfoliolinks, JSONArray co_portfolioLaguages) {
 
         try {
             //포폴 테이블에 있는 값이 존재할경우 업데이트
             Optional<CoPortfolio> coPortfolio1 = Optional.ofNullable(coPortfolioMapper.getCoPortfolio(coPortfolio.getCo_portfolioId()));
             if(coPortfolio1.isPresent()){
-                //if 본인이 맞을경우
-                coMyPageMapper.updateCoPortfolio(coPortfolio);
-                coMyPageMapper.updateCoLaguageOfPortfolio(coPortfolio);
-                coMyPageMapper.updateCoLinkOfPortfolio(coPortfolio);
+
+                Map<String, Object> coPortfolioDto = new HashMap<>();
+                this.coMyPageMapper.updateCoPortfolio(coPortfolio);
+
+                for (Object co_portfoliolaguage : co_portfolioLaguages){
+                    long co_laugageId = (long) co_portfoliolaguage;
+                    this.coMyPageMapper.updateCoLaguageOfPortfolio(coPortfolio.getCo_portfolioId(),co_laugageId);
+                }
+                JSONObject jsonObj;
+                for (Object co_portfoliolink : co_portfoliolinks){
+                    jsonObj = (JSONObject) co_portfoliolink;
+                    coPortfolioDto.put("co_portfolioId",coPortfolio.getCo_portfolioId());
+                    coPortfolioDto.put("co_link",jsonObj.get("co_link").toString());
+                    this.coMyPageMapper.updateCoLinkOfPortfolio(coPortfolioDto);
+                }
                 }
             return setResponse(200, "success", coPortfolio);
         } catch (Exception e) {
