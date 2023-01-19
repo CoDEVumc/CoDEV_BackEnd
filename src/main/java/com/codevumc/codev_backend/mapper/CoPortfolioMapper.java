8cbbe1e371bf8c1e0fadcfc3043b60ceb86d1539
wlsdbsrb <diejeadong@naver.com>
2023-01-19 10:25:08 +0900

    feat: 포트폴리오 조회

--- src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoPortfolioMapper.java
@@ -1,7 +1,13 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoHeartOfProject;
+import com.codevumc.codev_backend.domain.CoPortfolio;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Optional;
 
 @Mapper
 public interface CoPortfolioMapper {
+    Optional<CoPortfolio> getCoPortfolio(@Param("co_portfolioId") long co_portfolioId);
 }
--- src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioService.java
@@ -1,4 +1,7 @@
 package com.codevumc.codev_backend.service.co_portfolio;
 
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
+
 public interface CoPortfolioService {
+    CoDevResponse getCoPortfolio(long co_portfolioId);
 }
