e503809b7634f64dfaac4019c793633111c9b455
wlsdbsrb <diejeadong@naver.com>
2023-01-19 10:25:08 +0900

    feat: 포트폴리오 조회

--- src/main/java/com/codevumc/codev_backend/controller/co_portfolio/CoPortfolioController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_portfolio/CoPortfolioController.java
@@ -1,11 +1,16 @@
 package com.codevumc.codev_backend.controller.co_portfolio;
 
 import com.codevumc.codev_backend.controller.JwtController;
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.jwt.JwtTokenProvider;
 import com.codevumc.codev_backend.service.co_portfolio.CoPortfolioServiceImpl;
 import com.codevumc.codev_backend.service.co_user.JwtService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/p1/co_portfolio")
@@ -15,5 +20,9 @@ public class CoPortfolioController extends JwtController {
     public CoPortfolioController(JwtTokenProvider jwtTokenProvider, JwtService jwtService, CoPortfolioServiceImpl coPortfolioService) {
         super(jwtTokenProvider, jwtService);
         this.coPortfolioService = coPortfolioService;
+    }
+    @GetMapping("/{co_portfolioId}")
+    public CoDevResponse getPortfolio(HttpServletRequest request, @PathVariable("co_portfolioId") long co_portfolioId){
+        return coPortfolioService.getCoPortfolio(co_portfolioId);
     }
 }
--- src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_portfolio/CoPortfolioServiceImpl.java
@@ -1,13 +1,29 @@
 package com.codevumc.codev_backend.service.co_portfolio;
 
+import com.codevumc.codev_backend.domain.CoPortfolio;
+import com.codevumc.codev_backend.domain.CoProject;
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.mapper.CoPortfolioMapper;
 import com.codevumc.codev_backend.service.ResponseService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class CoPortfolioServiceImpl extends ResponseService implements CoPortfolioService {
     private final CoPortfolioMapper coPortfolioMapper;
+
+    @Override
+    public CoDevResponse getCoPortfolio(long co_portfolioId) {
+        try {
+            Optional<CoPortfolio> coPortfolio = coPortfolioMapper.getCoPortfolio(co_portfolioId);
+            return setResponse(200, "Complete", coPortfolio);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
 
