d0d1ad0e0a61286c61ff4642036bd5bc81844922
min9yu98 <kimmingyu9811@gmail.com>
2023-01-20 11:37:46 +0900

    fix: 포트폴리오 삭제 서비스 반환값 수정, 마이페이지용 포트폴리오 변수명 수정

--- src/main/java/com/codevumc/codev_backend/service/co_mypageportfolio/CoMyPagePortfolioService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_mypageportfolio/CoMyPagePortfolioService.java
@@ -1,12 +1,12 @@
-package com.codevumc.codev_backend.service.co_portfolio;
+package com.codevumc.codev_backend.service.co_mypageportfolio;
 
 import com.codevumc.codev_backend.domain.CoPortfolio;
 import org.json.simple.JSONArray;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 
-public interface CoPortfolioService {
+public interface CoMyPagePortfolioService {
     void insertCoPortfolio(CoPortfolio coPortfolio, JSONArray co_languages, JSONArray co_links);
     CoDevResponse getCoPortfolio(long co_portfolioId, String co_email);
     CoDevResponse updateCoPortfolio(CoPortfolio coPortfolio, JSONArray co_languages, JSONArray co_links);
-    CoDevResponse deletePortfolio(String co_email, long portfolioId);
+    CoDevResponse deletePortfolio(String co_email, long co_portfolioId);
 }
