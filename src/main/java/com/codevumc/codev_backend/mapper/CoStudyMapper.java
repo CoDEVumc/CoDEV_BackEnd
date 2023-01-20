517ba2d551b9e469a0cb330f52b40e2b7b15e5c2
min9yu98 <kimmingyu9811@gmail.com>
2023-01-20 15:11:01 +0900

    feat: 스터디 상세조회 API 완성

--- src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
@@ -1,8 +1,16 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoLanguage;
+import com.codevumc.codev_backend.domain.CoPhotos;
+import com.codevumc.codev_backend.domain.CoStudy;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface CoStudyMapper {
-
+    Optional<CoStudy> getCoStudy(long co_studyId);
+    List<CoLanguage> getCoLanguageList(long co_studyId);
+    long getCoHeartCount(long co_studyId);
 }
