7841e5dd11fa1e753d5067c430c76c6b57c6a941
simhani1 <simhani1@gmail.com>
2023-01-20 13:03:52 +0900

    fix: CoStudyHeartService 파일 수정

--- src/main/java/com/codevumc/codev_backend/service/co_studyheart/CoStudyHeartServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_studyheart/CoStudyHeartServiceImpl.java
@@ -1,6 +1,12 @@
 package com.codevumc.codev_backend.service.co_studyheart;
 
+import com.codevumc.codev_backend.mapper.CoStudyMapper;
 import com.codevumc.codev_backend.service.ResponseService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class CoStudyHeartServiceImpl extends ResponseService implements CoStudyHeartService {
+    private final CoStudyMapper coStudyMapper;
 }
