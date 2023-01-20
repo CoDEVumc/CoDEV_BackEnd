023b9e88f16e2aeb9e8a9529a9d474d7806c253f
simhani1 <simhani1@gmail.com>
2023-01-20 12:57:07 +0900

    fix: 스터디 관련 디렉토리 생성

--- src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyServiceImpl.java
@@ -1,6 +1,15 @@
 package com.codevumc.codev_backend.service.co_study;
 
+import com.codevumc.codev_backend.mapper.CoPhotosMapper;
+import com.codevumc.codev_backend.mapper.CoStudyMapper;
 import com.codevumc.codev_backend.service.ResponseService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class CoStudyServiceImpl extends ResponseService implements CoStudyService {
+    private final CoStudyMapper coStudyMapper;
+    private final CoPhotosMapper coPhotosMapper;
+
 }
