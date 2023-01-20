d4627a7b84acbff9315198164ac11870681f4214
wlsdbsrb <diejeadong@naver.com>
2023-01-20 14:19:05 +0900

    feat: 스터디 전체 조회(필터링)

--- src/main/java/com/codevumc/codev_backend/controller/co_study/CoStudyController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_study/CoStudyController.java
@@ -1,14 +1,19 @@
 package com.codevumc.codev_backend.controller.co_study;
 
 import com.codevumc.codev_backend.controller.JwtController;
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.jwt.JwtTokenProvider;
 import com.codevumc.codev_backend.service.co_file.CoFileServiceImpl;
 import com.codevumc.codev_backend.service.co_project.CoProjectServiceImpl;
 import com.codevumc.codev_backend.service.co_study.CoStudyServiceImpl;
 import com.codevumc.codev_backend.service.co_studyheart.CoStudyHeartServiceImpl;
 import com.codevumc.codev_backend.service.co_user.JwtService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/codev/study")
@@ -22,6 +27,11 @@ public class CoStudyController extends JwtController {
         this.coFileService = coFileService;
         this.coStudyService = coStudyService;
         this.coStudyHeartService = coStudyHeartService;
+    }
+
+    @GetMapping(value = "/studies")
+    public CoDevResponse getAllProjects(HttpServletRequest request, @RequestParam("coLocationTag") String coLocationTag, @RequestParam("coPartTag") String coPartTag, @RequestParam("coKeyword") String coKeyword, @RequestParam("coProcessTag") String coProcessTag) throws Exception {
+        return coStudyService.getCoStudies(getCoUserEmail(request), coLocationTag, coPartTag, coKeyword, coProcessTag);
     }
 
 
--- src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyServiceImpl.java
@@ -1,15 +1,43 @@
 package com.codevumc.codev_backend.service.co_study;
 
+import com.codevumc.codev_backend.domain.CoProject;
+import com.codevumc.codev_backend.domain.CoStudy;
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.mapper.CoPhotosMapper;
 import com.codevumc.codev_backend.mapper.CoStudyMapper;
 import com.codevumc.codev_backend.service.ResponseService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @Service
 public class CoStudyServiceImpl extends ResponseService implements CoStudyService {
     private final CoStudyMapper coStudyMapper;
     private final CoPhotosMapper coPhotosMapper;
+
+    private String setting(String keyword) {
+        return keyword == null ? null : "%" + keyword + "%";
+    }
+
+    @Override
+    public CoDevResponse getCoStudies(String co_email, String co_locationTag, String co_partTag, String co_keyword, String co_processTag) {
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("co_email", co_email);
+        condition.put("co_locationTag", co_locationTag);
+        condition.put("co_partTag", setting(co_partTag));
+        condition.put("co_keyword", setting(co_keyword));
+        condition.put("co_processTag", co_processTag);
+        List<CoStudy> coStudies = this.coStudyMapper.getCoStudies(condition);
+        try {
+            return setResponse(200, "success", coStudies);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
