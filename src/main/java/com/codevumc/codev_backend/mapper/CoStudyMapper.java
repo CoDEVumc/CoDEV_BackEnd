735682e45cf447190ded94865368291f9095f150
wlsdbsrb <diejeadong@naver.com>
2023-01-20 14:19:05 +0900

    feat: 스터디 전체 조회(필터링)

--- src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
@@ -1,8 +1,12 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoStudy;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface CoStudyMapper {
-
+    List<CoStudy> getCoStudies(Map<String, Object> condition);
 }
