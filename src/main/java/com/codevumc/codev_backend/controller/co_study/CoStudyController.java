53f799fbddb088ab627172df87249ad4f900ec74
simhani1 <simhani1@gmail.com>
2023-01-22 23:06:08 +0900

    fix: 스처디 도메인 변수명 수정, 글 작성 반환형 수정

--- src/main/java/com/codevumc/codev_backend/controller/co_study/CoStudyController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_study/CoStudyController.java
@@ -9,18 +9,11 @@ import com.codevumc.codev_backend.service.co_file.CoFileServiceImpl;
 import com.codevumc.codev_backend.service.co_study.CoStudyServiceImpl;
 import com.codevumc.codev_backend.service.co_studyheart.CoStudyHeartServiceImpl;
 import com.codevumc.codev_backend.service.co_user.JwtService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PatchMapping;
 import com.google.gson.Gson;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -67,7 +60,7 @@ public class CoStudyController extends JwtController {
                 .co_content(study.get("co_content").toString())
                 .co_process(CoStudy.DevType.from("ING"))
                 .co_part(study.get("co_part").toString())
-                .co_limit((Integer) study.get("co_limit"))
+                .co_total((Integer) study.get("co_total"))
                 .co_deadLine((study.get("co_deadLine").toString())).build();
         JSONParser parser = new JSONParser();
         Gson gson = new Gson();
