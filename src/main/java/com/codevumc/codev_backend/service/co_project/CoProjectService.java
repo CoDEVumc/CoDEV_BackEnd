8acbe5695c55b550e746e6faf07ec5e933752b09
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-01-24 16:57:48 +0900

    fix: CoProjectService comflict resolve

--- src/main/java/com/codevumc/codev_backend/service/co_project/CoProjectService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_project/CoProjectService.java
@@ -7,8 +7,8 @@ import org.json.simple.JSONArray;
 public interface CoProjectService {
     CoDevResponse insertProject(CoProject coProject, JSONArray co_languages, JSONArray co_parts);
     CoDevResponse updateProject(CoProject coProject, JSONArray co_lanugages, JSONArray co_parts);
-    CoDevResponse getCoProject(long co_projectId);
+    CoDevResponse getCoProject(String co_viewer, long co_projectId);
     void updateMainImg(String co_mainImg, long co_projectId);
-    CoDevResponse getCoProjects(String co_email, String co_locationTag, String co_partTag, String co_keyword, String co_processTag, int limit, int offset, int page);
+    CoDevResponse getCoProjects(String co_email, String co_locationTag, String co_partTag, String co_keyword, String co_sortingTag, String co_processTag, int limit, int offset, int page);
     CoDevResponse deleteCoProject(String co_email, long co_projectId);
 }
