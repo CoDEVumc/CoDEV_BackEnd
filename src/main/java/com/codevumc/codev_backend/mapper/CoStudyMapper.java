e9d9e34b8e7d6acf92dc32d6c244e310f16486ab
min9yu98 <kimmingyu9811@gmail.com>
2023-01-24 16:25:21 +0900

    feat: 스터디 지원 API 추가

--- src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
@@ -1,5 +1,6 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoRecruitOfStudy;
 import com.codevumc.codev_backend.domain.CoStudy;
 import com.codevumc.codev_backend.domain.CoLanguage;
 import com.codevumc.codev_backend.domain.CoHeartOfStudy;
@@ -26,5 +27,6 @@ public interface CoStudyMapper {
     List<CoStudy> getCoStudies(Map<String, Object> condition);
     boolean deleteCoStudy(Map<String, Object> studyDto);
     boolean getCoRecruitStatus(String co_viewer, long co_studyId);
-    boolean deleteRecruitOfStudy(Map<String, Object> recruitDto);
+    void deleteRecruitOfStudy(Map<String, Object> recruitDto);
+    void insertCoRecruitOfStudy(CoRecruitOfStudy coRecruitOfStudy);
 }
