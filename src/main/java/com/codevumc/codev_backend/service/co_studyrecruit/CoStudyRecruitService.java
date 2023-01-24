e63ab2ff8b130957b41dae3f005699b93e2255d4
min9yu98 <kimmingyu9811@gmail.com>
2023-01-24 14:49:10 +0900

    feat:스터디 지원 취소 API 추가

--- src/main/java/com/codevumc/codev_backend/service/co_studyrecruit/CoStudyRecruitService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_studyrecruit/CoStudyRecruitService.java
@@ -1,4 +1,7 @@
 package com.codevumc.codev_backend.service.co_studyrecruit;
 
+import com.codevumc.codev_backend.errorhandler.CoDevResponse;
+
 public interface CoStudyRecruitService {
+    CoDevResponse cancelRecruitStudy(String co_email, long co_studyId);
 }
