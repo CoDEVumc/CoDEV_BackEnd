facb6f95d99dea07e9dfb52e31986d0e605d3d6b
pinetree2 <pinetree3966@gmail.com>
2023-01-24 15:12:29 +0900

    fix: 코드 복구

--- src/main/java/com/codevumc/codev_backend/mapper/CoMyPageMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoMyPageMapper.java
@@ -1,10 +1,17 @@
 package com.codevumc.codev_backend.mapper;
 
 import com.codevumc.codev_backend.domain.CoPortfolio;
+import com.codevumc.codev_backend.domain.CoProject;
+import com.codevumc.codev_backend.domain.CoStudy;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface CoMyPageMapper {
-
+    List<Long> getCoHeartsOfStudy(String co_email);
+    List<CoStudy> getCoStudies(String co_email, long co_studyId);
+    List<Long> getCoHeartsOfProject(String co_email);
+    List<CoProject> getCoProjects(Long coProjectId);
 }
