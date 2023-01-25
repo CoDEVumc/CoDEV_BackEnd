47fe5ff2d9c071c15d3e7f4a413de5c7c10cf96e
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-01-25 23:00:09 +0900

    fix: 스터디 지원/ 지원취소/조회 수정

--- src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoStudyMapper.java
@@ -19,7 +19,6 @@ public interface CoStudyMapper {
     void insertCoLanguageOfStudy(long co_studyId, long co_languageId);
     void deleteCoLanguageOfStudy(long co_studyId);
     void updateCoMainImg(@Param("co_mainImg") String co_mainImg, @Param("co_studyId") long co_studyId);
-    void insertCoPartOfStudy(Map<String, Object>coPartDto);
     Optional<CoStudy> getCoStudy(long co_studyId);
     List<CoLanguage> getCoLanguageList(long co_studyId);
     long getCoHeartCount(long co_studyId);
@@ -28,10 +27,9 @@ public interface CoStudyMapper {
     void deleteCoHeartOfStudy(String co_email, Long co_studyId);
     List<CoStudy> getCoStudies(Map<String, Object> condition);
     boolean deleteCoStudy(Map<String, Object> studyDto);
-    boolean getCoRecruitStatus(String co_viewer, long co_studyId);
+    boolean getCoRecruitStatus(String co_email, long co_studyId);
     void insertCoRecruitOfStudy(CoRecruitOfStudy coRecruitOfStudy);
     void deleteRecruitOfStudy(Map<String, Object> recruitDto);
     String getCoHeartOfStudyEmail(Long co_studyId);
     List<CoRecruitOfStudy> getCoStudyApplicants(Map<String, Object> condition);
-    boolean isWriter(Map<String, Object> recruitDto);
 }
