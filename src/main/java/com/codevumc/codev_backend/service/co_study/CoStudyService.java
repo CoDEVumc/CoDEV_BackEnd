be8277e400d6c4d16a1a1b2ed05e0fc54696455f
min9yu98 <kimmingyu9811@gmail.com>
2023-01-27 15:10:34 +0900

    feat: 스터디 마감기한 연장 추가

--- src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_study/CoStudyService.java
@@ -11,4 +11,5 @@ public interface CoStudyService {
     CoDevResponse getCoStudies(String co_email, String co_locationTag, String co_partTag, String co_keyword, String co_sortingTag, String co_processTag, int limit, int offset, int page);
     CoDevResponse getCoStudy(String co_viewer, long co_studyId);
     CoDevResponse deleteStudy(String coUserEmail, long coStudyId);
+    CoDevResponse updateCoStudyDeadLine(CoStudy coStudy);
 }
