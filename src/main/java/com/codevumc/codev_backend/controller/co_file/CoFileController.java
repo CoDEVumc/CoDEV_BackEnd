68d2af00ec052c414e2114a393dc73272217d610
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-01-28 20:40:00 +0900

    feat: 서버관리자 파일업로드 기능 구현

--- src/main/java/com/codevumc/codev_backend/controller/co_file/CoFileController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_file/CoFileController.java
@@ -1,14 +1,20 @@
 package com.codevumc.codev_backend.controller.co_file;
 
 
+import com.codevumc.codev_backend.domain.CoPhotos;
 import com.codevumc.codev_backend.service.co_file.CoFileServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
@@ -24,6 +30,14 @@ public class CoFileController {
     @GetMapping("/image")
     public ResponseEntity<Resource> getImage(@RequestParam Map<String, String> param) {
         return coFileService.showImage(param);
+    }
+
+    @PostMapping(value = "/upload", consumes = {MediaType.ALL_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE})
+    public void uploadFile(@RequestPart(required = false) MultipartFile[] files) throws Exception {
+        List<CoPhotos> coPhotos = Arrays.asList(files)
+                .stream()
+                .map(file -> coFileService.storeFile(file, "ADMIN", "LANGUAGE"))
+                .collect(Collectors.toList());
     }
 
 
