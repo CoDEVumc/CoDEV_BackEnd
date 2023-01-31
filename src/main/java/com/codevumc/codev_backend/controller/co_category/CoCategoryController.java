1d18b659641d24c49d7e4454c9be8529620b936d
wlsdbsrb <diejeadong@naver.com>
2023-01-31 23:33:12 +0900

    feat: 기술 분야 조회 및 분야별 세부 기술 조회

--- src/main/java/com/codevumc/codev_backend/controller/co_category/CoCategoryController.java
+++ src/main/java/com/codevumc/codev_backend/controller/co_category/CoCategoryController.java
@@ -5,6 +5,7 @@ import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.service.co_category.CoCategoryServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,15 @@ public class CoCategoryController {
     @GetMapping("/location")
     public CoDevResponse getLocation() throws Exception {
         return coCategoryService.getLocation();
+    }
+
+    @GetMapping("/parts")
+    public CoDevResponse getParts() throws Exception {
+        return coCategoryService.getParts();
+    }
+
+    @GetMapping("/language")
+    public CoDevResponse getLanguageOfPart(@RequestParam("coPart") String co_part) throws Exception {
+        return coCategoryService.getLanguageOfPart(co_part);
     }
 }
