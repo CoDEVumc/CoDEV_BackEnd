4db8eee8e8bdb4d7ffec873b9566fee13ecdfd81
wlsdbsrb <diejeadong@naver.com>
2023-02-02 14:08:01 +0900

    feat: 지원자 포트폴리오 조회 도메인 매퍼 컨트롤러추가

---
+++ src/main/java/com/codevumc/codev_backend/domain/CoRecruitOfStudyPortfolio.java
@@ -0,0 +1,29 @@
+package com.codevumc.codev_backend.domain;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CoRecruitOfStudyPortfolio {
+    private long co_portfolioId;
+    private String co_email;
+    private String profileImg;
+    private String co_name;
+    private String co_gender;
+    private String co_birth;
+    private String co_title;
+    private String co_rank;
+    private String co_headLine;
+    private String co_introduction;
+    private String co_motivation;
+    private String co_links;
+    private List<CoLanguage> co_languageList;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
