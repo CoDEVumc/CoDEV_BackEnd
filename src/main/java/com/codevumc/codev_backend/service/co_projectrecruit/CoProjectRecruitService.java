9e145d9f4f212cd165405734ffcb8c71eeb396cf
simhani1 <simhani1@gmail.com>
2023-02-03 11:29:21 +0900

    feat: 상태값 조회 쿼리 추가

--- src/main/java/com/codevumc/codev_backend/service/co_projectrecruit/CoProjectRecruitService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_projectrecruit/CoProjectRecruitService.java
@@ -1,11 +1,9 @@
 package com.codevumc.codev_backend.service.co_projectrecruit;
 
-import com.codevumc.codev_backend.domain.CoApplicantInfo;
 import com.codevumc.codev_backend.domain.CoProject;
 import com.codevumc.codev_backend.domain.CoRecruitOfProject;
+import com.codevumc.codev_backend.domain.CoTempSaveApplicants;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
-
-import java.util.List;
 
 public interface CoProjectRecruitService {
     CoDevResponse insertCoRecruitOfProject(CoRecruitOfProject coRecruitOfProject);
@@ -13,5 +11,5 @@ public interface CoProjectRecruitService {
     CoDevResponse getCoApplicantsOfProject(String co_email, long co_projectId, String co_part);
     CoDevResponse closeCoProjectDeadLine(String co_email, Long co_projectId, CoProject co_applicantsList);
     CoDevResponse getCoPortfolioOfApplicant(String co_email, long co_projectId, long co_portfolioId);
-    CoDevResponse saveCoApplicantsTemporarily(String co_email, long co_projectId, List<CoApplicantInfo> coApplicantInfos);
+    CoDevResponse saveCoApplicantsTemporarily(String co_email, long co_projectId, CoTempSaveApplicants coTempSaveApplicants);
 }
