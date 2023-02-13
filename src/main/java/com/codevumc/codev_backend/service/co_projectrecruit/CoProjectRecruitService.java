cc8a91572de882ade3239ac520945704d041c140
wlsdbsrb <diejeadong@naver.com>
2023-02-13 20:55:16 +0900

    fix: 프로젝트 모집마감 수정

--- src/main/java/com/codevumc/codev_backend/service/co_projectrecruit/CoProjectRecruitService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_projectrecruit/CoProjectRecruitService.java
@@ -1,15 +1,15 @@
 package com.codevumc.codev_backend.service.co_projectrecruit;
 
-import com.codevumc.codev_backend.domain.CoProject;
 import com.codevumc.codev_backend.domain.CoRecruitOfProject;
 import com.codevumc.codev_backend.domain.CoTempSaveApplicants;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
+import org.json.simple.JSONArray;
 
 public interface CoProjectRecruitService {
     CoDevResponse insertCoRecruitOfProject(CoRecruitOfProject coRecruitOfProject);
     CoDevResponse cancelCoRecruitOfProject(CoRecruitOfProject coRecruitOfProject);
     CoDevResponse getCoApplicantsOfProject(String co_email, long co_projectId, String co_part);
-    CoDevResponse closeCoProjectDeadLine(String co_email, Long co_projectId, CoProject co_applicantsList);
+    CoDevResponse closeCoProjectDeadLine(String co_email, Long co_projectId, JSONArray co_applicantList);
     CoDevResponse getCoPortfolioOfApplicant(String co_email, long co_projectId, long co_portfolioId);
     CoDevResponse saveCoApplicantsTemporarily(String co_email, long co_projectId, CoTempSaveApplicants coTempSaveApplicants);
 }
