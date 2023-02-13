efc5af1e4001ee4b3b1e0630cdfe00046dba2f05
min9yu98 <kimmingyu9811@gmail.com>
2023-02-13 21:01:39 +0900

    fix: 스터디 모집 마감 코드 수정 완료

--- src/main/java/com/codevumc/codev_backend/service/co_studyrecruit/CoStudyRecruitService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_studyrecruit/CoStudyRecruitService.java
@@ -1,15 +1,15 @@
 package com.codevumc.codev_backend.service.co_studyrecruit;
 
 import com.codevumc.codev_backend.domain.CoRecruitOfStudy;
-import com.codevumc.codev_backend.domain.CoStudy;
 import com.codevumc.codev_backend.domain.CoTempSaveApplicants;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
+import org.json.simple.JSONArray;
 
 
 public interface CoStudyRecruitService {
     CoDevResponse cancelRecruitStudy(CoRecruitOfStudy coRecruitOfStudy);
     CoDevResponse submitCoStudy(CoRecruitOfStudy coRecruitOfStudy);
-    CoDevResponse completeCoStudyRecruitment(String co_email, long co_studyId, CoStudy co_applicantList);
+    CoDevResponse completeCoStudyRecruitment(String co_email, long co_studyId, JSONArray co_applicantList);
     CoDevResponse getCoApplicantsOfStudy(String co_email, long co_studyId, String co_part);
     CoDevResponse getCoPortfolioOfApplicant(String co_email,long co_studyId,long co_portfolioId);
     CoDevResponse saveCoApplicantsTemporarily(String co_email, long co_studyId, CoTempSaveApplicants coTempSaveApplicants);
