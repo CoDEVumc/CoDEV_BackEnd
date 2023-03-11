deb85492af1cd2c29d38b4a37cab617d9c4814c6
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-03-11 19:00:32 +0900

    feat : 게시판 글쓰기 (사진) 추가

--- src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
@@ -1,7 +1,13 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoQnaBoard;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface CoQnaBoardMapper {
+
+    void updateCoMainImg(@Param("co_mainImg") String co_mainImg, @Param("co_qnaId") long co_qnaId);
+
+    void insertCoQnaBoard(CoQnaBoard coQnaBoard);
 }
