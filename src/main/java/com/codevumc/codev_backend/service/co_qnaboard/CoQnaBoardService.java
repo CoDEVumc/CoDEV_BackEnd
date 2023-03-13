a7a63db74330b84fbf3825b8a70b5ef0f71fe01e
pinetree2 <pinetree3966@gmail.com>
2023-03-13 16:39:48 +0900

    feat: 댓글달기 기능

--- src/main/java/com/codevumc/codev_backend/service/co_qnaboard/CoQnaBoardService.java
+++ src/main/java/com/codevumc/codev_backend/service/co_qnaboard/CoQnaBoardService.java
@@ -1,10 +1,12 @@
 package com.codevumc.codev_backend.service.co_qnaboard;
 
 
+import com.codevumc.codev_backend.domain.CoCommentOfQnaBoard;
 import com.codevumc.codev_backend.domain.CoQnaBoard;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 
 public interface CoQnaBoardService {
     void updateMainImg(String co_mainImg, long co_qnaId);
     CoDevResponse insertCoQnaBoard(CoQnaBoard coQnaBoard);
+    CoDevResponse insertCoCommentOfQnaBoard(CoCommentOfQnaBoard coCommentOfQnaBoard);
 }
