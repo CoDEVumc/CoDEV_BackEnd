b64c72d61eecdadafc96400e86e6331ea7e7ec9a
min9yu98 <kimmingyu9811@gmail.com>
2023-03-14 00:47:23 +0900

    feat: 질문 대댓글 작성 api 추가

--- src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
@@ -2,6 +2,7 @@ package com.codevumc.codev_backend.mapper;
 
 import com.codevumc.codev_backend.domain.CoCommentOfQnaBoard;
 import com.codevumc.codev_backend.domain.CoQnaBoard;
+import com.codevumc.codev_backend.domain.CoReCommentOfQnaBoard;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -11,4 +12,5 @@ public interface CoQnaBoardMapper {
     void updateCoMainImg(@Param("co_mainImg") String co_mainImg, @Param("co_qnaId") long co_qnaId);
     void insertCoQnaBoard(CoQnaBoard coQnaBoard);
     void insertCoCommentOfQnaBoard(CoCommentOfQnaBoard coCommentOfQnaBoard);
+    void insertCoReCommentOfQnaBoard(CoReCommentOfQnaBoard coReCommentOfQnaBoard);
 }
--- src/main/java/com/codevumc/codev_backend/service/co_qnaboard/CoQnaBoardServiceImpl.java
+++ src/main/java/com/codevumc/codev_backend/service/co_qnaboard/CoQnaBoardServiceImpl.java
@@ -3,6 +3,7 @@ package com.codevumc.codev_backend.service.co_qnaboard;
 import com.codevumc.codev_backend.domain.CoCommentOfQnaBoard;
 import com.codevumc.codev_backend.domain.CoInfoBoard;
 import com.codevumc.codev_backend.domain.CoQnaBoard;
+import com.codevumc.codev_backend.domain.CoReCommentOfQnaBoard;
 import com.codevumc.codev_backend.errorhandler.AuthenticationCustomException;
 import com.codevumc.codev_backend.errorhandler.CoDevResponse;
 import com.codevumc.codev_backend.errorhandler.ErrorCode;
@@ -51,6 +52,14 @@ public class CoQnaBoardServiceImpl extends ResponseService implements CoQnaBoard
         }
     }
 
-
-
+    @Override
+    public CoDevResponse insertCoReCommentOfQnaBoard(CoReCommentOfQnaBoard coReCommentOfQnaBoard) {
+        try {
+            this.coQnaBoardMapper.insertCoReCommentOfQnaBoard(coReCommentOfQnaBoard);
+            return setResponse(200, "message", "질문게시판 대댓글이 작성되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new AuthenticationCustomException(ErrorCode.REQUESTFAILED);
+        }
+    }
 }
