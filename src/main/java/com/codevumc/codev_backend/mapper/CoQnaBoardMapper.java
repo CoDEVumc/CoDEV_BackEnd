70fbceb1fd53454bfab83d257c9f2330047b2264
pinetree2 <pinetree3966@gmail.com>
2023-03-13 16:39:48 +0900

    feat: 댓글달기 기능

--- src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoQnaBoardMapper.java
@@ -1,5 +1,6 @@
 package com.codevumc.codev_backend.mapper;
 
+import com.codevumc.codev_backend.domain.CoCommentOfQnaBoard;
 import com.codevumc.codev_backend.domain.CoQnaBoard;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,6 +9,6 @@ import org.apache.ibatis.annotations.Param;
 public interface CoQnaBoardMapper {
 
     void updateCoMainImg(@Param("co_mainImg") String co_mainImg, @Param("co_qnaId") long co_qnaId);
-
     void insertCoQnaBoard(CoQnaBoard coQnaBoard);
+    void insertCoCommentOfQnaBoard(CoCommentOfQnaBoard coCommentOfQnaBoard);
 }
