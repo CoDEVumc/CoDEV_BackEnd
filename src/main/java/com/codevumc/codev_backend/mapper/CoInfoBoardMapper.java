1f60e611413e32e8ee600511b6d00d60c5dc0fed
이해송 <79689822+pinetree2@users.noreply.github.com>
2023-03-20 19:48:34 +0900

    fix: 이름 혼동 수정

--- src/main/java/com/codevumc/codev_backend/mapper/CoInfoBoardMapper.java
+++ src/main/java/com/codevumc/codev_backend/mapper/CoInfoBoardMapper.java
@@ -17,7 +17,7 @@ public interface CoInfoBoardMapper {
 
     void updateCoMainImg(@Param("co_mainImg") String co_mainImg, @Param("co_infoId") long co_infoId);
     void insertCoInfoBoard(CoInfoBoard coInfoBoard);
-    void insertCoCommentOfQnaBoard(CoCommentOfInfoBoard coCommentOfInfoBoard);
+    void insertCoCommentOfInfoBoard(CoCommentOfInfoBoard coCommentOfInfoBoard);
     void insertCoReCommentOfInfoBoard(CoReCommentOfInfoBoard coReCommentOfInfoBoard);
     Long getCoMarkOfInfoBoardEmail(@Param("co_email") String co_email, @Param("co_infoId") long co_infoId);
     void insertCoMarkOfInfoBoard(String co_email, long co_infoId);
